f812feab54c1932fef409670ce67a670ef2fe8c4
lishenglun <1830558461@qq.com>
2019-06-26 21:17:04 +0800

    git_02_修改

--- git-Test-02/src/Test02.java
+++ git-Test-02/src/Test02.java
@@ -3,5 +3,6 @@ public class Test02 {
         System.out.println("Hello Word!");
         System.out.println("Hello Word!");
         System.out.println("Hello Word!");
+        System.out.println("作出了修改");
     }
 }
